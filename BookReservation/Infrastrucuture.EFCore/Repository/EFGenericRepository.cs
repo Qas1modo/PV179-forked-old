ca4fbf338f74a40b106f5a758e1c614a51c8f565
Richad Lacek <xlacek@fi.muni.cz>
2022-10-18 15:17:52 +0200

    added arguments checkers

--- BookReservation/Infrastrucuture.EFCore/Repository/EFGenericRepository.cs
+++ BookReservation/Infrastrucuture.EFCore/Repository/EFGenericRepository.cs
@@ -22,28 +22,53 @@ namespace Infrastrucure.EFCore.Repository
             dbSet = context.Set<TEntity>();
         }
 
-        public virtual TEntity? GetByID(object id)
+        public virtual TEntity GetByID(object id)
         {
-            return dbSet.Find(id);
+            if (id == null)
+            {
+                throw new Exception("Argument Id is null.");
+            }
+
+            TEntity? entity = dbSet.Find(id);
+            if (entity == null)
+            {
+                throw new Exception("Entity with given Id does not exist.");
+            }
+            return entity;
         }
 
         public virtual void Insert(TEntity entity)
         {
+            if (entity == null)
+            {
+                throw new Exception("Arugment entity is null");
+            }
+
             dbSet.Add(entity);
         }
 
         public virtual void Delete(object id)
         {
+            if (id == null)
+            {
+                throw new Exception("Argument Id is null.");
+            }
+
             TEntity? entityToDelete = dbSet.Find(id);
             if (entityToDelete == null)
             {
-                return;
+                throw new Exception("Entity with given Id does not exist.");
             }
             Delete(entityToDelete);
         }
 
         public virtual void Delete(TEntity entityToDelete)
         {
+            if (entityToDelete == null)
+            {
+                throw new Exception("Argument entityToDelete is null.");
+            }
+
             if (context.Entry(entityToDelete).State == EntityState.Detached)
             {
                 dbSet.Attach(entityToDelete);
@@ -53,6 +78,11 @@ namespace Infrastrucure.EFCore.Repository
 
         public virtual void Update(TEntity entityToUpdate)
         {
+            if (entityToUpdate == null)
+            {
+                throw new Exception("Argument entityToUpdate is null.");
+            }
+
             dbSet.Attach(entityToUpdate);
             context.Entry(entityToUpdate).State = EntityState.Modified;
         }
