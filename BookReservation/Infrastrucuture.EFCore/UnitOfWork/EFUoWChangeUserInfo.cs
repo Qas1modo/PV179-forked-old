7d24291062479974989ea009bee9a48ac0bae89e
Kristián Gutič <xgutic@fi.muni.cz>
2022-10-21 21:33:30 +0200

    Adding implementation of UoWChangeUserInfo for EF

---
+++ BookReservation/Infrastrucuture.EFCore/UnitOfWork/EFUoWChangeUserInfo.cs
@@ -0,0 +1,48 @@
+﻿using DAL.Models;
+using DAL;
+using Infrastructure.UnitOfWork;
+using Infrastrucure.Repository;
+using Infrastrucure.EFCore.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EFCore.UnitOfWork
+{
+    public class EFUoWChangeUserInfo : IUoWChangeUserInfo
+    {
+        private IRepository<User> userRepository;
+
+        public BookReservationDbContext Context { get; }
+
+        public EFUoWChangeUserInfo(DbContextOptions<BookReservationDbContext> options)
+        {
+            Context = new(options);
+        }
+
+        public EFUoWChangeUserInfo()
+        {
+            Context = new();
+        }
+
+        public IRepository<User> UserRepository
+        {
+            get
+            {
+                if (userRepository == null)
+                {
+                    userRepository = new EFGenericRepository<User>(Context);
+                }
+                return userRepository;
+            }
+        }
+
+        public async Task Commit()
+        {
+            await Context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
