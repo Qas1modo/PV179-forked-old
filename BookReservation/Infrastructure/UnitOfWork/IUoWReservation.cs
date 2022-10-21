1767536ea696ff4a1edcdfc68192bc1d384faf1e
Kristián Gutič <xgutic@fi.muni.cz>
2022-10-21 23:30:13 +0200

    Adding UoW for Reservation

---
+++ BookReservation/Infrastructure/UnitOfWork/IUoWReservation.cs
@@ -0,0 +1,10 @@
+﻿using DAL.Models;
+using Infrastrucure.Repository;
+
+namespace Infrastructure.UnitOfWork
+{
+    public interface IUoWReservation : IUnitOfWork
+    {
+        IRepository<Rent> RentRepository { get; }
+    }
+}
