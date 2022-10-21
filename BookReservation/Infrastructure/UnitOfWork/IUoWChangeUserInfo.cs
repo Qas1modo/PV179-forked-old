6bee0435a330da48f17ff418d5d99112376ee6d8
Kristián Gutič <xgutic@fi.muni.cz>
2022-10-21 21:32:06 +0200

    Adding UoW interface for changing user information

---
+++ BookReservation/Infrastructure/UnitOfWork/IUoWChangeUserInfo.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Models;
+using Infrastrucure.Repository;
+
+
+namespace Infrastructure.UnitOfWork
+{
+    public interface IUoWChangeUserInfo : IUnitOfWork
+    {
+        IRepository<User> UserRepository { get; }
+
+    }
+}
