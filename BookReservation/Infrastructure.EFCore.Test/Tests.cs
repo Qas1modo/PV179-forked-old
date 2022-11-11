7ff8bfd1608b2ab82f86f218379afc3e4fb8046f
Richad Lacek <xlacek@fi.muni.cz>
2022-11-11 13:57:55 +0100

    Introduced new abstract class Tests for setting up in memory db

---
+++ BookReservation/Infrastructure.EFCore.Test/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.EFCore.Test
+{
+    public abstract class Tests
+    {
+        protected readonly DbContextOptions<BookReservationDbContext> dbContextOptions;
+
+        public Tests()
+        {
+            var myDatabaseName = "mydatabase_" + DateTime.Now.ToFileTimeUtc();
+
+            dbContextOptions = new DbContextOptionsBuilder<BookReservationDbContext>()
+                .UseInMemoryDatabase(databaseName: myDatabaseName)
+                .Options;
+
+            InitDatabase();
+        }
+
+        public abstract void InitDatabase();
+    }
+}
+
