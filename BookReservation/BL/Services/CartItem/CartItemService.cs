89141d36cdaf2cca39719b22d54ecd8452955e7d
Richad Lacek <xlacek@fi.muni.cz>
2022-11-14 20:28:26 +0100

    added cart item service

---
+++ BookReservation/BL/Services/CartItem/CartItemService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using AutoMapper;
+using BL.DTOs.BasicDtos;
+using BL.Services.CRUD;
+using DAL;
+using Infrastructure.EFCore.UnitOfWork;
+using Infrastructure.UnitOfWork;
+
+namespace BL.Services.CartItem
+{
+	public class CartItemService : ICartItemService
+	{
+        private readonly IMapper _mapper;
+        private readonly BookReservationDbContext _context;
+
+        public CartItemService(BookReservationDbContext context, IMapper mapper)
+		{
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public void AddItem(CartItemDto itemDto)
+        {
+            using (IUoWCartItem uow = new EFUoWCartItem(_context))
+            {
+                var cartItemRepo = new CRUDService<DAL.Models.CartItem>(uow.CartItemRepository, _mapper);
+                cartItemRepo.Create<CartItemDto>(itemDto);
+                uow.Commit();
+            }
+        }
+
+        public void RemoveItem(object id)
+        {
+            using (IUoWCartItem uow = new EFUoWCartItem(_context))
+            {
+                var cartItemRepo = new CRUDService<DAL.Models.CartItem>(uow.CartItemRepository, _mapper);
+                cartItemRepo.DeleteById(id);
+                uow.Commit();
+            }
+        }
+    }
+}
+
---
+++ BookReservation/Infrastrucuture.EFCore/UnitOfWork/EFUoWCartItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DAL;
+using DAL.Models;
+using Infrastructure.EFCore.Repository;
+using Infrastructure.Repository;
+using Infrastructure.UnitOfWork;
+
+namespace Infrastructure.EFCore.UnitOfWork
+{
+	public class EFUoWCartItem : IUoWCartItem
+	{
+        private IRepository<CartItem> cartItemRepository;
+
+        public BookReservationDbContext Context { get; }
+
+        public EFUoWCartItem(BookReservationDbContext context)
+		{
+            Context = context;
+        }
+
+        public IRepository<CartItem> CartItemRepository
+        {
+            get
+            {
+                if (cartItemRepository == null)
+                {
+                    cartItemRepository = new EFGenericRepository<CartItem>(Context);
+                }
+                return cartItemRepository;
+            }
+        }
+
+        public async Task Commit()
+        {
+            await Context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+    }
+}
+
