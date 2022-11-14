7f35e4849914654987a5efd99bf33bbc0b728c55
Richad Lacek <xlacek@fi.muni.cz>
2022-11-14 19:28:17 +0100

    added review Service

---
+++ BookReservation/BL/Services/Review/ReviewService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AutoMapper;
+using DAL;
+using Infrastructure.UnitOfWork;
+using BL.DTOs.BasicDtos;
+using Infrastructure.EFCore.UnitOfWork;
+using DAL.Models;
+using BL.Services.CRUD;
+
+namespace BL.Services.Review
+{
+    public class ReviewService : IReviewService
+	{
+        private readonly IMapper _mapper;
+		private readonly BookReservationDbContext _context;
+
+        public ReviewService(BookReservationDbContext context, IMapper mapper)
+		{
+			_mapper = mapper;
+			_context = context;
+		}
+
+		public void AddReview(ReviewDto reviewDto)
+		{
+			using (IUoWReview uow = new EFUoWReview(_context))
+			{
+				var reviewRepo = new CRUDService<DAL.Models.Review>(uow.ReviewRepository, _mapper);
+				reviewRepo.Create<ReviewDto>(reviewDto);
+				uow.Commit();
+			}
+        }
+	}
+}
