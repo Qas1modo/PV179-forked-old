34c24fa0cc63e12cdcffd6aed837c27a105cfd43
Kristián Gutič <xgutic@fi.muni.cz>
2022-12-09 13:24:15 +0100

    Adding service

--- BookReservation/WebAppMVC/Controllers/MainPageController.cs
+++ BookReservation/WebAppMVC/Controllers/MainPageController.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BL.Services.StockServ;
+using Microsoft.AspNetCore.Mvc;
+using BL.DTOs;
+using WebAppMVC.Models;
 
 namespace WebAppMVC.Controllers
 {
     public class MainPageController : Controller
     {
+        private readonly IStockService stockService;
+
+        public MainPageController(IStockService stockService)
+        {
+            this.stockService = stockService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+
+            //var filter = new BookFilterDto
+            //{
+            //    OnStock = true,
+            //    Page = 1
+            //};
+
+            //var model = new MainPageIndexModel()
+            //{
+            //    Items = stockService.ShowBooks(filter).Items;
+            //};
+
+			return View();
         }
     }
 }
